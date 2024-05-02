817f6963a2eb016208c8d43dc0c48e3496e6a469
webgurusam <shyamghosh666@gmail.com>
2024-05-02 21:27:39 +0600

    added news tab

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('index.js')
+const handleCategory = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    const categories = data.data.news_category;
+    displayCategory(categories);
+}
+const displayCategory =(categories) => {
+    console.log(categories);    
+    const tabContents = document.getElementById('tab-contents');
+    categories.slice(0,3).forEach(category => {
+        const div = document.createElement('div');
+        div.innerHTML = `<a role="tab" class="tab">${category.category_name}</a>`;
+        tabContents.appendChild(div);
+    })
+}
+handleCategory();
